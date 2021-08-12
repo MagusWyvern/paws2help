a6ee2d127fcfb7f058bba73a618cbf3a773e5891
MagusWyvern <72914828+MagusWyvern@users.noreply.github.com>
2021-08-12 16:55:44 +0800

    added some code comments and dispersed animals

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ var userlongitude = 0;
 var mymap = 0;
 
 function getLocation() {
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-        alert('500 Error');
+        alert('Geolocation is not supported by this browser.');
     }
 }
 
 function loadMap() {
 
     mymap = L.map('mapid').setView([userlatitude, userlongitude], 15);
-    console.log(userlatitude)
     
-
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -24,27 +23,21 @@ function loadMap() {
         zoomOffset: -1,
         accessToken: 'pk.eyJ1IjoibWFndXN3eXZlcm4iLCJhIjoiY2tzNGFweDNrMDFpMzJwbWxpZmlmMHhmciJ9.Itc6X_zrrrRfUj7GwwXP8w'
     }).addTo(mymap);
-    
-    var marker = L.marker([2.734557, 101.951923]);
+
+    // Circle marker, used for stray animals
     
     var circle = L.circle([2.707465, 101.946087], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 500
+        radius: 100
     }).addTo(mymap);
     
-    // var polygon = L.polygon([
-    //     [2.742101, 101.907806],
-    //     [2.703007, 101.89579],
-    //     [2.720668, 101.885662]
-    // ]).addTo(mymap);
-    
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
     circle.bindPopup("Stray cats around this area.");
-    // polygon.bindPopup("I am a polygon.");
     
     var popup = L.popup();
+
+    // Debugging coords, comment out this section of code for production
     
     function onMapClick(e) {
         popup
@@ -54,6 +47,8 @@ function loadMap() {
     }
     
     mymap.on('click', onMapClick);
+
+    // Initialize the custom icons
     
     var dogIcon = L.icon({
         iconUrl: 'dog-solid.svg',
@@ -98,16 +93,35 @@ function loadMap() {
         shadowAnchor: [4, 62],  // the same for the shadow
         popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
     });
+
+    // Add the markers to the map
+
+    // Veterinar clinics
     
     L.marker([2.734557, 101.951923], { icon: helpIcon }).addTo(mymap).bindPopup("Klinik Veterinar XYZ Seremban is offering help.");
+
+    L.marker([5.399737, 101.678467], { icon: helpIcon }).addTo(mymap).bindPopup("Klinik Veterinar XYZ Seremban is offering help.");   
+
+    // Cats
     
-    L.marker([2.730442, 101.929092], { icon: catIcon }).addTo(mymap).bindPopup("Pn. Aminah is donating cats.");
+    L.marker([5.182659, 100.942383], { icon: catIcon }).addTo(mymap).bindPopup("Pn. Aminah is donating cats.");
     
-    L.marker([2.733357, 101.925316], { icon: catIcon }).addTo(mymap).bindPopup("En. Adam is looking for cats.");
+    L.marker([2.320514, 103.117676], { icon: catIcon }).addTo(mymap).bindPopup("En. Adam is looking for cats.");
+
+    L.marker([3.734101, 102.365112], { icon: catIcon }).addTo(mymap).bindPopup("En. Adam is looking for cats.");
+
+    // Dogs
     
-    L.marker([2.713638, 101.965141], { icon: dogIcon }).addTo(mymap).bindPopup("Ms. Cheoo is donating dogs.");
+    L.marker([4.985423, 102.414551], { icon: dogIcon }).addTo(mymap).bindPopup("Ms. Cheoo is donating dogs.");
+
+    L.marker([4.29493, 102.282715], { icon: dogIcon }).addTo(mymap).bindPopup("Ms. Cheoo is donating dogs.");
+
+    // Birds
     
-    L.marker([2.710209, 101.926174], { icon: birdIcon }).addTo(mymap).bindPopup("En. Adam is looking for birds.");
+    L.marker([4.174806, 101.535645], { icon: birdIcon }).addTo(mymap).bindPopup("En. Adam is looking for birds.");
+
+    L.marker([4.448272, 102.689209], { icon: birdIcon }).addTo(mymap).bindPopup("En. Adam is looking for birds.");
+
 
     L.marker([userlatitude, userlongitude]).addTo(mymap).bindPopup("You are here!");
 }
@@ -116,13 +130,17 @@ function showPosition(position) {
 
     userlatitude = position.coords.latitude;
     userlongitude = position.coords.longitude;
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
+
+    // Debugging
+    // console.log(position.coords.latitude);
+    // console.log(position.coords.longitude);
+
     loadMap();
 
 }
 
-getLocation()
+// Get user location on arrival
+getLocation();
 
 
 
