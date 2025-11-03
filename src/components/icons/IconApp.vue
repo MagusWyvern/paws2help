94428f47e2e94ccedfc648cc72aa70afd5ba2285
MagusWyvern <maguswyvern@gmail.com>
2025-11-03 09:41:45 +0800

    Nuke all old project files

--- src/components/icons/IconApp.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <img src="/icon.png">
-</template>
-
-<style scoped>
-img {
-    width: 6em;
-}
-</style>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/MarkerCluster.css'
-import './assets/leaflet.css'
-
-createApp(App).mount('#app')
