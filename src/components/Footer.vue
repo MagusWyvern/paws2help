897669836227bb9cb9cfe08ae8157c6aa3430f39
MagusWyvern <maguswyvern@gmail.com>
2026-02-27 12:41:56 +0800

    incorrect privacy policy link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -23,7 +23,7 @@
                 <a href="https://www.apache.org/licenses/LICENSE-2.0">Apache 2.0</a>. The website content
                 is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
             </p>
-            <a href="./privacy-policy.html">Privacy Policy</a>
+            <a href="./privacy.html">Privacy Policy</a>
         </div>
     </footer>
 </template>
